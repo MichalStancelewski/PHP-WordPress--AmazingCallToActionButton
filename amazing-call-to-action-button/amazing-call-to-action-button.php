0cfb675b68073b5f7f56b8d3e130b5d9d1dc4607
Michał Stancelewski <michal@stancelewski.pl>
2022-07-17 23:32:53 +0200

    Initial plugin file

---
+++ amazing-call-to-action-button/amazing-call-to-action-button.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Amazing Call To Action Button
+ * Plugin URI: https://cta.stancelewski.pl/
+ * Description: Create fully customizable Call To Action buttons on your website.
+ * Version: 0.1
+ * Author: Michał Stancelewski
+ * Author URI: https://stancelewski.pl/
+ **/
